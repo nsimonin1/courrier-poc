207927a96aff7b0d632a83084428ba96e564aad8
Simon Pascal Ngos <Simon Pascal Ngos>
2016-08-16 21:51:30 +0400

    init project

---
+++ src/main/java/org/poc/CourrierPocApplication.java
@@ -0,0 +1,12 @@
+package org.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourrierPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourrierPocApplication.class, args);
+	}
+}
